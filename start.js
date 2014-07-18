7d75f3c8eb6b5174487a65cf625b516518f9dc43
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2014-07-18 21:53:16 +0200

    minor refactoring

--- start.js
+++ start.js
@@ -1,22 +1,22 @@
 var log = require('./utils/log.js');
 var auto = require('./lib/automato.js');
-
 auto.user = 'rough'
 auto.hosts = ['10.232.132.100', '10.232.132.100'];
 
-function httpdConfTransformer(file, host) {
+function httpdConfNameTransformer(file, host) {
     return 'work/bilanciatori/collaudo/' + host + '/' + file;
-    //return file;
 }
 
+log.info('deploy avviato');
 auto.gitPull(
     'http://rguidi:raffaele@10.238.11.11/gitbucket/git/rguidi/bilanciatori.git', 
     'work/bilanciatori', 
     function (stdout) {
-        auto.run('mkdir /home/rough/ciccio',null,
+        log.info('repo aggiornato');
+        auto.run('mkdir /home/rough/ciccio', null,
             function(){
                 log.info('cartelle create, inizio l\'upload');
-                auto.uploadAll('httpd.conf', httpdConfTransformer, '/home/rough/ciccio/httpd.conf', null, 
+                auto.upload('httpd.conf', httpdConfNameTransformer, '/home/rough/ciccio/httpd.conf', null, 
                     function() {
                         log.info('upload terminato');
                         auto.run('uname -a', null, function(res) {
